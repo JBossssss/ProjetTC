fc15186f5ead739b3d7374ee15282d59a1c865be
LoreLHR <125083061+LoreLHR@users.noreply.github.com>
2023-05-02 13:15:59 +0200

    Update mainV8.py

--- mainV8.py
+++ mainV8.py
@@ -1,13 +1,10 @@
-# -- coding: utf-8 --
 """
 Created on Tue Apr 18 14:03:01 2023
-
 @author: Loren
 """
 from CodeV8 import *
 from ProjUtilsV8 import *
 import streamlit as st
-
 
 st.set_page_config(page_title='Filtres RCAO',layout="wide",page_icon='📈',initial_sidebar_state="collapsed")
 
@@ -17,48 +14,29 @@ test=True
 data=[]
 
 side=st.sidebar
-tabs = st.tabs(['filtre 1','filtre 2','filtre 3','filtre 4','filtre 5'])
+
+# tabs = st.tabs(['filtre 1','filtre 2','filtre 3','filtre 4','filtre 5'])
+# for i in range(len(tabs)-1):
+#     with tabs[i+1]:
+#        st.caption("Veuillez d'abord créer le filtre précédant avant de créer celui-ci")
 cont1=side.container()
 cont2=side.container()
 p=1
-while(test==True):
+# while(test==True):
 
-    test=False
-    with tabs[p-1]:
-        sel,image,name,dat=filter(p)
-        if (sauvegarder(p)):
-            test=True
-            with cont1:
-                save_side(sel,image,name, dat, p)
-            tb=[sel,image,name, dat, p]
-            data.append(tb)
-            p=p+1
-    # Filtre='Filtre '
-    # Filtre+=str(p)
-    # st.session_state["tabs"].append(Filtre)
+    # test=False
+        # with tabs[p-1]:  
+sel,image,name,dat,plot=filter(p)
+if (sauvegarder(p)):
+    test=True
+    with cont1:
+        save_side(sel,image,name, dat, p)
+    tb=[sel,image,name, dat, p,plot]
+    data.append(tb)
+    p=p+1
+    st.write(plot)
 with cont2:
     if p!=1:
         download_pdf(data)
 
 
-
-
-
-
-
-
-#""Pour ajouter des tabs dynamic""
-
-# if "tabs" not in st.session_state:
-#     st.session_state["tabs"] = ["Filter Data", "Raw Data", "📈 Chart"]
-    # if "tabs" not in st.session_state:
-    #     st.session_state["tabs"] = ['Filtre 1']
-    # tabs = st.tabs(st.session_state["tabs"])
-
-
-# tabs = st.tabs(st.session_state["tabs"])
-
-#new_tab = st.text_input("Tab label", "New Tab")
-# if st.button("Add tab"):
-#     st.session_state["tabs"].append(new_tab)
-#     st.experimental_rerun()
